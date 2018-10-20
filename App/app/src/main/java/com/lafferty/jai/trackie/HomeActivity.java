d3f4dede4fbac4231eea54ae1469ddad66628279
Jai Lafferty <jai.lafferty@gmail.com>
2018-10-21 01:55:44 +1100

    Working side menu, GraphView API Testing

--- App/app/src/main/java/com/lafferty/jai/trackie/HomeActivity.java
+++ App/app/src/main/java/com/lafferty/jai/trackie/HomeActivity.java
@@ -1,13 +1,145 @@
 package com.lafferty.jai.trackie;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.jjoe64.graphview.DefaultLabelFormatter;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import java.util.Date;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+        Toolbar toolbar = (Toolbar)findViewById(R.id.mainToolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_white);
+        CreateGraph();
+    }
+
+
+    public void CreateGraph(){
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+
+        Date d1 = new Date(1538352000*1000);
+        Date d2 = new Date(1538438400*1000);
+        Date d3 = new Date(1538524800*1000);
+        Date d4 = new Date(1538724800*1000);
+        Date d5 = new Date(1538824800*1000);
+
+
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(d1, 1),
+                new DataPoint(d2, 5),
+                new DataPoint(d3, 3),
+                new DataPoint(d4, 10),
+                new DataPoint(d5, 50),
+
+        });
+        graph.addSeries(series);
+        StyleSeries(series);
+        StyleGraph(graph);
+    }
+
+    public void StyleSeries(LineGraphSeries<DataPoint> series){
+        series.setDrawDataPoints(true);
+        series.setDataPointsRadius(10);
+        series.setColor(R.color.colorPrimary);
+    }
+
+    public void StyleGraph(GraphView graph){
+        graph.getViewport().setScrollable(false); // enables horizontal scrolling
+        graph.getViewport().setScrollableY(false); // enables vertical scrolling
+        graph.getViewport().setScalable(false); // enables horizontal zooming and scrolling
+        graph.getViewport().setScalableY(false); // enables vertical zooming and scrolling
+        graph.setTitle("Weight");
+        graph.getGridLabelRenderer().setHorizontalAxisTitle("Time");
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this));
+        graph.getGridLabelRenderer().setHumanRounding(false);
+        graph.getGridLabelRenderer().setGridStyle(GridLabelRenderer.GridStyle.NONE);
+        graph.getGridLabelRenderer().setPadding(20);
+
+        graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
+            @Override
+            public String formatLabel(double value, boolean isValueX) {
+                if (isValueX) {
+                    // show normal x values
+                    return new DateAsXAxisLabelFormatter(HomeActivity.this).formatLabel(value, true);
+                } else {
+                    // show currency for y values
+                    return super.formatLabel(Math.round(value), isValueX);
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_share:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                Toast.makeText(this,"hihi",Toast.LENGTH_SHORT).show();
+
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
